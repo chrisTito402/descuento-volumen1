14989eb77ddb44873d44facd35161ec9019a1e96
Christopher Alvarez Centeno <christopher.alvarez251954@potros.itson.edu.mx>
2026-09-23 09:25:33 -0700

    Fix volume discount boundary

--- descuento-volumen/src/test/java/mx/itson/devops/descuento/DescuentoFronteraTest.java
+++ descuento-volumen/src/test/java/mx/itson/devops/descuento/DescuentoFronteraTest.java
@@ -33,4 +33,10 @@ class DescuentoFronteraTest {
         int obtenido = descuento.porcentaje(101);
         assertEquals(10, obtenido);
     }
+
+    @Test
+    void calculaTotalConDescuentoEnCentavos() {
+        long obtenido = descuento.totalCentavos(1000, 100);
+        assertEquals(90000, obtenido);
+    }
 }
